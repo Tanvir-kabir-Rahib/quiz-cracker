b1b264336e324681d174c4a184390015f96e9984
Tanvir Kabir <tanvir@Tanvirs-MacBook-Pro.local>
2022-10-11 21:56:44 +0600

    404 page done

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import notfound404 from "../../404-not-found.webp"
+import "./NotFound.css"
 
 const NotFound = () => {
     return (
-        <div>
-            <h1>Page Not Found.</h1>
+        <div>            
+            <p className='text-center text-4xl font-bold'>Page Not Found</p>
+            <img src={notfound404} alt=""/>
+            <p className='text-center text-3xl font-semibold'>Return <Link className='underline link' to="/home">Home</Link></p>
         </div>
     );
 };
