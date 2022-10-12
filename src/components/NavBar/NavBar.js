d501149637fcf022572fed60c5224a5828196ce8
Tanvir Kabir <tanvir@Tanvirs-MacBook-Pro.local>
2022-10-12 21:58:45 +0600

    responsive navbar margin added

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -22,16 +22,16 @@ const NavBar = () => {
                 </div>
                 <div className={`text-xl font-semibold bg-white flex flex-col md:flex-row absolute md:static ease-in duration-300 z-10 ${open ? "h-full right-8 top-20" : "right-8 top-[-144px]"}`}>
                     <NavLink className={({ isActive }) =>
-                        isActive ? "mx-5 logo-color underline underline-offset-8" : "mx-5 logo-color"
+                        isActive ? "mx-5 logo-color underline underline-offset-4 md:underline-offset-8 mb-2 md:mb-0" : "mx-5 logo-color mb-2 md:mb-0"
                     } to="/home">Home</NavLink>
                     <NavLink className={({ isActive }) =>
-                        isActive ? "mx-5 logo-color underline underline-offset-8" : "mx-5 logo-color"
+                        isActive ? "mx-5 logo-color underline underline-offset-4 md:underline-offset-8 mb-2 md:mb-0" : "mx-5 logo-color mb-2 md:mb-0"
                     } to="/topics">Topics</NavLink>
                     <NavLink className={({ isActive }) =>
-                        isActive ? "mx-5 logo-color underline underline-offset-8" : "mx-5 logo-color"
+                        isActive ? "mx-5 logo-color underline underline-offset-4 md:underline-offset-8 mb-2 md:mb-0" : "mx-5 logo-color mb-2 md:mb-0"
                     } to="/statistics">Statistics</NavLink>
                     <NavLink className={({ isActive }) =>
-                        isActive ? "mx-5 logo-color underline underline-offset-8" : "mx-5 logo-color"
+                        isActive ? "mx-5 logo-color underline underline-offset-4 md:underline-offset-8 mb-2 md:mb-0" : "mx-5 logo-color mb-2 md:mb-0"
                     } to="/blogs">Blog</NavLink>
                 </div>
             </div>
