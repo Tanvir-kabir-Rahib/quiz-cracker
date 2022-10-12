9109a1bb2a308a39b0c69ba875cd7614e804d212
Tanvir Kabir <tanvir@Tanvirs-MacBook-Pro.local>
2022-10-12 15:03:21 +0600

    active routing added

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -3,7 +3,7 @@ import favicon from '../../favicon.webp'
 import { NavLink } from 'react-router-dom';
 import "./NavBar.css"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars,faXmark } from '@fortawesome/free-solid-svg-icons';
+import { faBars, faXmark } from '@fortawesome/free-solid-svg-icons';
 
 const NavBar = () => {
     const [open, setOpen] = useState(false);
@@ -21,10 +21,18 @@ const NavBar = () => {
                     }
                 </div>
                 <div className={`text-xl font-semibold bg-white flex flex-col md:flex-row absolute md:static ease-in duration-300 z-10 ${open ? "h-full right-8 top-20" : "right-8 top-[-144px]"}`}>
-                    <NavLink className="mx-5 logo-color" to="/home">Home</NavLink>
-                    <NavLink className="mx-5 logo-color" to="/topics">Topics</NavLink>
-                    <NavLink className="mx-5 logo-color" to="/statistics">Statistics</NavLink>
-                    <NavLink className="mx-5 logo-color" to="/blogs">Blog</NavLink>
+                    <NavLink className={({ isActive }) =>
+                        isActive ? "mx-5 logo-color underline underline-offset-8" : "mx-5 logo-color"
+                    } to="/home">Home</NavLink>
+                    <NavLink className={({ isActive }) =>
+                        isActive ? "mx-5 logo-color underline underline-offset-8" : "mx-5 logo-color"
+                    } to="/topics">Topics</NavLink>
+                    <NavLink className={({ isActive }) =>
+                        isActive ? "mx-5 logo-color underline underline-offset-8" : "mx-5 logo-color"
+                    } to="/statistics">Statistics</NavLink>
+                    <NavLink className={({ isActive }) =>
+                        isActive ? "mx-5 logo-color underline underline-offset-8" : "mx-5 logo-color"
+                    } to="/blogs">Blog</NavLink>
                 </div>
             </div>
         </nav>
